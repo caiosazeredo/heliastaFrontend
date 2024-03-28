6d79ac3b5203351d5e4f73514b5693b7fd4919f0
Fellipe97 <lfalmeida97@hotmail.com>
2024-03-28 00:27:13 -0300

    feat: :art: aparencia do login

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,6 +1,10 @@
+import { Container, FormDiv, ImgDiv, ImgsPatrocinioDiv, LeftSlide, PatrocinioDiv, RightSlide, TitleDiv, TitlePatrocinioDiv } from "./styles";
 
-
-
+import logoImage from "../../assets/logo.png";
+import imgLogin from "../../assets/imgLogin.svg";
+import imgPesc from "../../assets/imgPesc.svg";
+import imgCapgov from "../../assets/imgCapgov.svg";
+import { Button, TextField } from "@mui/material";
 
 
 
@@ -8,7 +12,54 @@
 
 
 export const Login = () => {
-    return(
-        <h1>LOGIN</h1>
+    return (
+        <Container>
+            <LeftSlide>
+
+                <TitleDiv>
+                    <img src={logoImage} alt="Logo" />
+                    <h1>Heliasta</h1>
+                </TitleDiv>
+
+                <FormDiv>
+                    <TextField
+                        hiddenLabel
+                        placeholder="Email"
+                        id="filled-hidden-label-normal"
+                        variant="filled"
+                        className="inputStyle"
+                        type="email"
+                    />
+                    <TextField
+                        hiddenLabel
+                        placeholder="Senha"
+                        id="filled-hidden-label-normal"
+                        variant="filled"
+                        className="inputStyle"
+                        type="password"
+
+                    />
+                    <Button variant="contained" className="buttonStyle">Login</Button>
+
+                </FormDiv>
+
+            </LeftSlide>
+
+
+            <RightSlide>
+                <ImgDiv>
+                    <img src={imgLogin} alt="Logo" />
+                </ImgDiv>
+                <PatrocinioDiv>
+                    <TitlePatrocinioDiv>
+                        <h1>Desenvolvimento:</h1>
+                    </TitlePatrocinioDiv>
+                    <ImgsPatrocinioDiv>
+                        <img src={imgPesc} alt="Logo" />
+                        <img src={imgCapgov} alt="Logo" />
+                    </ImgsPatrocinioDiv>
+                </PatrocinioDiv>
+            </RightSlide>
+        </Container>
     )
 }
---
+++ src/pages/Login/styles.ts
@@ -0,0 +1,108 @@
+import styled from 'styled-components';
+
+
+export const Container = styled.div`
+    display: flex;
+    height: 100vh;
+`
+export const LeftSlide = styled.div`
+    background-color: #F3F4F6;
+    display: flex;
+    flex-direction: column;
+
+    gap: 50px;
+
+    justify-content: center;
+    align-items: center;
+
+    width: 35%;
+
+    box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+
+    @media (max-width: 1300px) {
+        width: 100%;
+    }
+`
+export const RightSlide = styled.div`
+    flex: 1;
+`
+
+export const TitleDiv = styled.div`
+    display: flex;
+
+    justify-content: center;
+    align-items: center;
+
+    gap: 20px;
+
+    h1{
+        color: #1976D2;
+        font-size: 40px;
+    }
+    img{
+        width: 48px;
+        height: 48px;
+
+        //filter: brightness(0) saturate(100%) invert(13%) sepia(98%) saturate(6484%) hue-rotate(68deg) brightness(95%) contrast(90%);
+    }
+`
+
+export const FormDiv = styled.form`
+    display: flex;
+    flex-direction: column;
+
+    .inputStyle{
+        width: 350px;
+        padding: 10px;
+    }
+
+    .buttonStyle{
+        align-self: center;
+
+        max-width: 330px;
+        width: 330px;
+        padding: 15px;
+
+        font-size: 14px;
+
+        margin-top: 20px;
+    }
+`
+
+export const ImgDiv = styled.div`
+    flex: 1;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+
+    img{
+        width: 70%;
+    }
+
+    @media (max-width: 1300px) {
+        display: none; 
+    }
+`
+export const PatrocinioDiv = styled.div`
+    position: absolute;
+    bottom: 0;
+    right: 0;
+
+    padding: 20px;
+`
+export const TitlePatrocinioDiv = styled.div`
+    h1{
+        font-size: 18px;
+        font-weight: 100;
+        margin-bottom: 10px;
+    }
+`
+export const ImgsPatrocinioDiv = styled.div`
+    display: flex;
+
+    img{
+        width: 120px;
+        height: 120px;
+    }
+`
