f2d5ac83ed692244fbf66401fc18e3bfdc20d5a8
Fellipe97 <lfalmeida97@hotmail.com>
2024-04-04 18:54:33 -0300

    feat: :sparkles: garantir que a tab muda sempre que entrar na home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,6 @@
+import { useEffect } from "react"
 
+import { useTabMenuContext } from '../../contexts/TabMenuContext';
 
 
 
@@ -6,6 +8,14 @@
 
 
 export const Home = () => {
+    const { handleChangeTabMenuContext } = useTabMenuContext();
+
+    
+    //garantir que o tab acompanha o usuário que colocar a rota na url e não navegar por click
+    useEffect(()=>{
+        handleChangeTabMenuContext(null, 0);
+    },[])
+
     return(
         <h1>HOME</h1>
     )
