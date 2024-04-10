1b49b43a051ba52bf08ed91ba21dff341f5bd4a8
Fellipe97 <lfalmeida97@hotmail.com>
2024-04-10 00:47:51 -0300

    feat: :construction: construindo a tela home de processos

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,8 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 
 import { useTabMenuContext } from '../../helpers/TabMenuContext';
-import { Container } from "./styles";
+import { BootstrapInput, Container, FilterDiv, HeaderDiv, ItemStatisticsInputsDiv, StatisticsAndFilterDiv, StatisticsDiv, StatisticsInputsDiv, StyledTableCell, StyledTableRow } from "./styles";
+
 
 
 
@@ -12,14 +13,45 @@ export const Home = () => {
     const { handleChangeTabMenuContext } = useTabMenuContext();
 
     
-    //garantir que o tab acompanha o usuário que colocar a rota na url e não navegar por click
-    useEffect(()=>{
-        handleChangeTabMenuContext(null, 0);
-    },[])
 
-    return(
+    //garantir que o tab acompanha o usuário que colocar a rota na url e não navegar por click
+    useEffect(() => {
+        handleChangeTabMenuContext(null, 0);
+    }, [])
+
+    return (
         <Container>
-            <h1>HOME</h1>
-        </Container>
+            <HeaderDiv>
+                <h1>Processos</h1>
+            </HeaderDiv>
+
+            <StatisticsAndFilterDiv>
+
+                <StatisticsDiv>
+                    <h1>Estatísticas</h1>
+
+                    <StatisticsInputsDiv>
+                        <ItemStatisticsInputsDiv>
+                            <h1>Total de Processos</h1>
+                            <BootstrapInput /* defaultValue="react-bootstrap" */ value={302} id="bootstrap-input" disabled />
+                        </ItemStatisticsInputsDiv>
+
+                        <ItemStatisticsInputsDiv>
+                            <h1>Total de Processos</h1>
+                            <BootstrapInput /* defaultValue="react-bootstrap" */ value={1352} id="bootstrap-input" disabled />
+                        </ItemStatisticsInputsDiv>
+                    </StatisticsInputsDiv>
+
+
+                </StatisticsDiv>
+                <FilterDiv>
+
+                </FilterDiv>
+            </StatisticsAndFilterDiv>
+
+            
+        
+
+        </Container >
     )
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,3 +1,4 @@
+import { InputBase, TableCell, TableRow, alpha, styled as styled2, tableCellClasses } from '@mui/material';
 import styled from 'styled-components';
 
 
@@ -5,11 +6,139 @@ export const Container = styled.div`
     display: flex;
     flex-direction: column;
 
-    overflow-y: auto;
-    min-height: 100vh;
+    flex: 1;
     max-height: 100vh;
-
+    overflow-y: auto;
+    
     @media (max-width: 1000px) {
         overflow-y: visible;
     }
 `
+
+export const HeaderDiv = styled.div`
+    border-bottom: 2px solid #E5E7EB;
+    padding: 15px 35px;
+    
+    align-items: center;
+
+    justify-content: space-between;
+
+    display: flex;
+
+    h1{
+        font-weight: 100;
+        font-size: 18px;
+    }
+
+    min-height: 70px;
+`
+
+export const StatisticsAndFilterDiv = styled.div`
+    padding: 25px 35px;
+    border-bottom: 2px solid #E5E7EB;
+`
+
+export const StatisticsDiv = styled.div`
+    h1{
+        font-weight: 100;
+        font-size: 14px;
+    }
+`;
+
+export const StatisticsInputsDiv = styled.div`
+    margin-top: 30px;
+    display: flex;
+    gap: 25px;
+
+
+    .inputStyle{
+        width: 250px;
+        font-size: 18px;
+    }
+
+    @media (max-width: 600px) {
+        align-items: center;
+        justify-content: center;
+        flex-direction: column;
+    }
+`;
+
+export const FilterDiv = styled.div`
+`;
+
+export const ItemStatisticsInputsDiv = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 5px;
+`;
+
+
+
+
+
+
+
+
+
+export const BootstrapInput = styled2(InputBase)(({ theme }) => ({
+    'label + &': {
+      marginTop: theme.spacing(3),
+    },
+    '& .MuiInputBase-input': {
+      borderRadius: 4,
+      position: 'relative',
+      backgroundColor: theme.palette.mode === 'light' ? '#F3F6F9' : '#1A2027',
+      border: '1px solid',
+      borderColor: theme.palette.mode === 'light' ? '#E0E3E7' : '#2D3843',
+      fontSize: 16,
+      cursor: 'not-allowed',
+      width: 'auto',
+      padding: '10px 12px',
+      transition: theme.transitions.create([
+        'border-color',
+        'background-color',
+        'box-shadow',
+      ]),
+      // Use the system font instead of the default Roboto font.
+      fontFamily: [
+        '-apple-system',
+        'BlinkMacSystemFont',
+        '"Segoe UI"',
+        'Roboto',
+        '"Helvetica Neue"',
+        'Arial',
+        'sans-serif',
+        '"Apple Color Emoji"',
+        '"Segoe UI Emoji"',
+        '"Segoe UI Symbol"',
+      ].join(','),
+      '&:focus': {
+        boxShadow: `${alpha(theme.palette.primary.main, 0.25)} 0 0 0 0.2rem`,
+        borderColor: theme.palette.primary.main,
+      },
+    },
+  }));
+
+
+
+
+
+export  const StyledTableCell = styled2(TableCell)(({ theme }) => ({
+    [`&.${tableCellClasses.head}`]: {
+      backgroundColor: theme.palette.common.black,
+      color: theme.palette.common.white,
+    },
+    [`&.${tableCellClasses.body}`]: {
+      fontSize: 14,
+    },
+  }));
+  
+export  const StyledTableRow = styled2(TableRow)(({ theme }) => ({
+    '&:nth-of-type(odd)': {
+      backgroundColor: theme.palette.action.hover,
+    },
+    // hide last border
+    '&:last-child td, &:last-child th': {
+      border: 0,
+    },
+  }));
